ef933dc0f1b475cfd0edc54dc43f0a04ec28142d
Leda <ledathemis@protonmail.com>
2022-05-05 07:10:58 +0000

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <header id="header">
+      <h1 id="header--title">Memory Card</h1>
+    </header>
+  );
+};
+
+export default Header;

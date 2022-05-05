3bcb3090abd59966cd8684ba749de76792c8460b
Leda <ledathemis@protonmail.com>
2022-05-05 08:00:45 +0000

    move cardsData to separate file

---
+++ src/cardsData.js
@@ -0,0 +1,10 @@
+const cardsData = [
+  { id: 1, title: 'Card 1', url: 'https://via.placeholder.com/200x300.png?text=Card%201' },
+  { id: 2, title: 'Card 2', url: 'https://via.placeholder.com/200x300.png?text=Card%202' },
+  { id: 3, title: 'Card 3', url: 'https://via.placeholder.com/200x300.png?text=Card%203' },
+  { id: 4, title: 'Card 4', url: 'https://via.placeholder.com/200x300.png?text=Card%204' },
+  { id: 5, title: 'Card 5', url: 'https://via.placeholder.com/200x300.png?text=Card%205' },
+  { id: 6, title: 'Card 6', url: 'https://via.placeholder.com/200x300.png?text=Card%206' },
+];
+
+export default cardsData;

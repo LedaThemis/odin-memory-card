b88d902a9c98d5b7a480efc0b0c37c55a7255dfa
Leda <ledathemis@protonmail.com>
2022-05-05 07:16:12 +0000

    use Scoreboard component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import Scoreboard from './Scoreboard';
+
 import '../styles/Header.css';
 
-const Header = () => {
+const Header = (props) => {
+  const { currentScore, bestScore } = props;
+
   return (
     <header id="header">
       <h1 id="header--title">Memory Card</h1>
+      <Scoreboard currentScore={currentScore} bestScore={bestScore} />
     </header>
   );
 };

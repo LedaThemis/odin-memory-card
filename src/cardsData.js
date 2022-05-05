a85fad48632a9fe65a49d390825c95feb6f35be8
Leda <ledathemis@protonmail.com>
2022-05-05 08:45:34 +0000

    add more cards

--- src/cardsData.js
+++ src/cardsData.js
@@ -5,6 +5,12 @@ const cardsData = [
   { id: 4, title: 'Card 4', url: 'https://via.placeholder.com/200x300.png?text=Card%204' },
   { id: 5, title: 'Card 5', url: 'https://via.placeholder.com/200x300.png?text=Card%205' },
   { id: 6, title: 'Card 6', url: 'https://via.placeholder.com/200x300.png?text=Card%206' },
+  { id: 7, title: 'Card 7', url: 'https://via.placeholder.com/200x300.png?text=Card%207' },
+  { id: 8, title: 'Card 8', url: 'https://via.placeholder.com/200x300.png?text=Card%208' },
+  { id: 9, title: 'Card 9', url: 'https://via.placeholder.com/200x300.png?text=Card%209' },
+  { id: 10, title: 'Card 10', url: 'https://via.placeholder.com/200x300.png?text=Card%2010' },
+  { id: 11, title: 'Card 11', url: 'https://via.placeholder.com/200x300.png?text=Card%2011' },
+  { id: 12, title: 'Card 12', url: 'https://via.placeholder.com/200x300.png?text=Card%2012' },
 ];
 
 export default cardsData;
